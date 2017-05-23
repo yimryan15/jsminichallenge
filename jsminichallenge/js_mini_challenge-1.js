a7a515f5260215c20349ae15c1eedb566c87395d
ryim-lv <ryim@learnvest.com>
2017-05-22 23:44:31 -0400

    updated prob 5 to alerts

--- jsminichallenge/js_mini_challenge-1.js
+++ jsminichallenge/js_mini_challenge-1.js
@@ -16,13 +16,13 @@ console.log(favThings[0][1]);
 console.log(favThings[1][2]);
 
 //Problem 3
-  if (num3 < 100) {
-    alert("The number is less than 100");
-  } else if (number > 100) {
-    alert("The number " + num3 + " is greater than 100");
-  } else {
-    alert("Your input of " + num3 +  " is not a valid number");
-  }
+if (num3 < 100) {
+  alert("The number is less than 100");
+} else if (number > 100) {
+  alert("The number " + num3 + " is greater than 100");
+} else {
+  alert("Your input of " + num3 +  " is not a valid number");
+}
 
 //Problem 4
 function enterName(name) {
@@ -34,13 +34,13 @@ enterName("ryan");
 //Problem 5
 function prizeDoor(door) {
   if (door == 1) {
-    console.log("You have selected door " + door + " and you won a car");
+    alert("You have selected door " + door + " and you won a car");
   } else if (door == 2 ) {
-    console.log("You have selected door " + door + " and you won a boat");
+    alert("You have selected door " + door + " and you won a boat");
   } else if (door == 3) {
-    console.log("You have selected door " + door + " and you won a millon dollars");
+    alert("You have selected door " + door + " and you won a millon dollars");
   } else {
-    console.log("Door number " + door + " is invalid");
+    alert("Door number " + door + " is invalid");
   }
 };
 
